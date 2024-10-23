d3d73bcdb387568f99c61b62da519d044ac29eb1
KOSASIH <kosasihg88@gmail.com>
2024-10-23 19:49:16 +0700

    Create development.js

---
+++ config/env/development.js
@@ -0,0 +1,19 @@
+export default {
+    db: {
+        host: 'localhost',
+        port: 5432,
+        database: 'dae_dev',
+        user: 'dev_user',
+        password: 'dev_password',
+    },
+    blockchain: {
+        network: 'localhost',
+        port: 8545,
+    },
+    api: {
+        baseUrl: 'http://localhost:3000/api',
+    },
+    logging: {
+        level: 'debug',
+    },
+};
