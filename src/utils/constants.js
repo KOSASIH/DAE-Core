f056c0c61c83551acd404080a731716ae72b6af3
KOSASIH <kosasihg88@gmail.com>
2024-10-23 19:37:26 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,29 @@ export const CONSTANTS = {
     MAX_TOKEN_TRANSFER: 1000000, // Maximum tokens that can be transferred in a single transaction
     MIN_VOTE_THRESHOLD: 100,      // Minimum votes required for a proposal to be considered
     DEFAULT_GAS_LIMIT: 2000000,   // Default gas limit for transactions
-    SUPPORTED_CHAINS: ['Ethereum', 'Binance Smart Chain'], // Supported blockchain networks
+    SUPPORTED_CHAINS: [
+        'Ethereum',                 // Main Ethereum network
+        'Binance Smart Chain',      // BSC mainnet
+        'Polygon',                  // Polygon (Matic) network
+        'Avalanche',                // Avalanche network
+        'Fantom',                   // Fantom Opera network
+        'Cardano',                  // Cardano network
+        'Solana',                   // Solana network
+        'Tezos',                    // Tezos network
+        'Tron',                     // Tron network
+        'EOS',                      // EOS network
+        'Algorand',                 // Algorand network
+        'Near',                     // Near Protocol
+        'Harmony',                  // Harmony network
+        'Arbitrum',                 // Arbitrum Layer 2
+        'Optimism',                 // Optimism Layer 2
+        'Zilliqa',                  // Zilliqa network
+        'Kusama',                   // Kusama network
+        'Polkadot',                 // Polkadot network
+        'Terra',                    // Terra network
+        'Waves',                    // Waves network
+        'Heco',                     // Huobi Eco-Chain
+        'Celo',                     // Celo network
+        'IoTeX',                    // IoTeX network
+    ], 
 };
