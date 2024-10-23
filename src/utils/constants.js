3c14250811b0d2c757752a623d5ee3896e1b9a3a
KOSASIH <kosasihg88@gmail.com>
2024-10-23 19:35:06 +0700

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+/**
+ * Constant values used throughout the project.
+ */
+export const CONSTANTS = {
+    MAX_TOKEN_TRANSFER: 1000000, // Maximum tokens that can be transferred in a single transaction
+    MIN_VOTE_THRESHOLD: 100,      // Minimum votes required for a proposal to be considered
+    DEFAULT_GAS_LIMIT: 2000000,   // Default gas limit for transactions
+    SUPPORTED_CHAINS: ['Ethereum', 'Binance Smart Chain'], // Supported blockchain networks
+};
